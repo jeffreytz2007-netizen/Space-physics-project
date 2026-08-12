22654ad7471b62b28af527faf7d0da112b7e211c
jeffreytz2007-netizen <jeffreytz2007@gmail.com>
2026-08-12 10:54:46 +0800

    Update omni_testing.py

--- omni_testing.py
+++ omni_testing.py
@@ -1,14 +1,15 @@
-#import os
+import bokeh
+print(bokeh.__version__)
+#%%
+#import pyspedas
+#from pytplot import tplot, tplot_names
 
-# Set where PySPEDAS should cache/download data
-#os.environ["SPEDAS_DATA_DIR"] = r"C:\spedas_data"
-#os.makedirs(os.environ["SPEDAS_DATA_DIR"], exist_ok=True)
-
-#print("SPEDAS_DATA_DIR =", os.environ.get("SPEDAS_DATA_DIR"))
-#print("Contents before load =", os.listdir(os.environ["SPEDAS_DATA_DIR"]))
-
-import pyspedas
-from pyspedas import tplot
 #omni_vars = pyspedas.projects.omni.data(trange=['2013-11-5', '2013-11-6'])
+
+#print("Loaded variables:")
 #print(omni_vars)
-tplot(['BX_GSE', 'BY_GSE', 'BZ_GSE', 'flow_speed', 'Vx', 'Vy', 'Vz', 'SYM_H'])
+
+#print("All tplot names:")
+#print(tplot_names())
+
+#tplot(omni_vars)
